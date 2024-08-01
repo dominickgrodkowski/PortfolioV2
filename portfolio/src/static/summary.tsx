f85432f402f9762be8b11dc846e023bf3e7e3eb1
Dominick Grodkowski <Dominick.Grodkowski@gmx.de>
2024-08-01 17:10:47 +0200

    first commit

---
+++ portfolio/src/static/summary.tsx
@@ -0,0 +1,10 @@
+function Summary(){
+    return (<>
+        <p className="text-4xl mb-8">About me</p>
+        <article className="desktop:text-xl mobile:text-md" itemScope itemType="https://schema.org/author">
+            <p className="mb-5">I&apos;m a fullstack developer with a bachelors degree  in Social Economics, specializing in Business Administration and Marketing🎓. So far I&apos;ve had the privilege of working at Europcar Mobility Group and the Freie und Hansestadt Hamburg. My main focus these days is building high-performance web applications for desktop and mobile devices using the latest technologies.</p>
+        </article>
+    </>);
+}
+
+export default Summary;
